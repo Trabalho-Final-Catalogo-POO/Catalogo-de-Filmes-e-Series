53782bbc77e2c1df2066af4d9f7ea8172475fbf2
Arthur Henry Martins Brito <131808877+aTu44@users.noreply.github.com>
2025-05-23 16:34:46 -0300

    Create Favoritos.cs
    Classe Favoritos do projeto

---
+++ Favoritos.cs
@@ -0,0 +1,22 @@
+class Favoritos
+{
+
+    private List<Midia> listaFavoritos = new List<Midia>();
+
+    public void AdicionarNaLista(Midia midia)
+    {
+        listaFavoritos.Add(midia);
+        Console.WriteLine($"'{midia.Nome}' foi adicionado aos favoritos.");
+    }
+
+    public void ExibirFavoritos()
+    {
+        Console.WriteLine("***** Lista de Favoritos *****");
+        foreach (Midia m in listaFavoritos)
+        {
+            Console.WriteLine(m);
+        }
+    }
+
+
+}
