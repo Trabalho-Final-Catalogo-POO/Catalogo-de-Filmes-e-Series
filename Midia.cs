cc17e6fddda1b7a26c74c3c1f8375a9a9f616e33
Vladimir Valentim <164585255+Vladimir0406@users.noreply.github.com>
2025-06-10 14:53:54 -0300

    Update Midia.cs

--- Midia.cs
+++ Midia.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Runtime.Intrinsics.Arm;
-using static OperaçoesUsuario;
-
 public class Midia
 {
     public string Nome { get; set; }
