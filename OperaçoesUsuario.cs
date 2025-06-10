6b2bbf377016f405b5e2332849b14b983cb8e6b5
Vladimir Valentim <164585255+Vladimir0406@users.noreply.github.com>
2025-06-10 14:35:43 -0300

    Update OperaçoesUsuario.cs

--- OperaçoesUsuario.cs
+++ OperaçoesUsuario.cs
@@ -1,53 +1,5 @@
 class OperaçoesUsuario
 {
-    public class Usuario
-    {
-        private string Nome;
-        public string _Nome
-        {
-            get { return Nome; }
-            set // Impede a modificação de um usuário existente sem um metódo
-            {
-                if (Nome == null)
-                    Nome = value;
-            }
-        }
-        private string Senha;
-        public string _Senha
-        {
-            get { return Senha; }
-            set // Impede a modificação de uma senha existente sem um metódo
-            {
-                if (Senha == null)
-                    Senha = value;
-            }
-        }
-        public Favoritos favoritos { get; private set; } = new Favoritos();
-
-        public Usuario() { }
-        public Usuario(string Nome, string Senha)
-        {
-            this.Senha = Senha;
-            this.Nome = Nome;
-        }
-        public void NovoNome(string Nome)
-        {
-            this.Nome = Nome;
-        }
-        public bool VerificaSenha(string Senha)
-        {
-            if (string.IsNullOrWhiteSpace(Senha))
-                Console.WriteLine("A senha não pode ser nula");
-            else if (Senha.Length < 6)
-                Console.WriteLine("Senha muito curta");
-            else
-                return true;
-
-            return false;
-        }
-        public void AtualizarSenha(string Senha) { this.Senha = Senha; }
-    }
-
     List<Usuario> Usuarios = new List<Usuario>();
 
     public string Cadastrar()
